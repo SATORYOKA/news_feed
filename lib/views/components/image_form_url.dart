bcfab164903b58c661de37043e9040907698b055
Sato Ryoka <satoryoka@mac.local>
2021-01-17 09:46:28 +0900

    add network image

---
+++ lib/views/components/image_form_url.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+import 'package:cached_network_image/cached_network_image.dart';
+
+class ImageFromUrl extends StatelessWidget {
+  final String imageUrl;
+  const ImageFromUrl({this.imageUrl});
+  @override
+  Widget build(BuildContext context) {
+    final isInvalidUrl = imageUrl.startsWith("http");
+    if (imageUrl == null || imageUrl == '' || !isInvalidUrl) {
+      print('error ===========================================$isInvalidUrl');
+      return const Icon(Icons.broken_image);
+    } else {
+      print('success ===========================================$imageUrl');
+      return CachedNetworkImage(
+        imageUrl: imageUrl,
+        placeholder: (context, url) => CircularProgressIndicator(),
+        errorWidget: (context, url, error) => Icon(Icons.error),
+      );
+    }
+  }
+}
